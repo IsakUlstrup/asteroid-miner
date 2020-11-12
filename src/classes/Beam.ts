1b26066744ded7c6abeb41badc1e1dd2bd480a65
Isak <isakulstrup@gmail.com>
2020-11-12 13:53:46 +0100

    added fake perspective effect to laser beam

--- src/classes/Beam.ts
+++ src/classes/Beam.ts
@@ -10,17 +10,33 @@ export default class Beam extends CanvasObject {
     this.position.y = y;
     this.intensity = intensity;
   }
-  draw(ctx: CanvasRenderingContext2D, canvasWidth: number, canvasHeight: number) {
-    ctx.strokeStyle = `rgba(255, 0, 0, ${this.intensity})`;
+  draw(
+    ctx: CanvasRenderingContext2D,
+    canvasWidth: number,
+    canvasHeight: number
+  ) {
+    ctx.fillStyle = `rgba(255, 0, 0, ${this.intensity})`;
     ctx.lineWidth = this.intensity * 5;
     ctx.lineCap = "round";
+
+    const perspective = 0.5;
+
     ctx.beginPath();
+    ctx.lineJoin = "round";
     ctx.moveTo(this.position.x, this.position.y);
     ctx.lineTo(
-      this.target.position.x * canvasWidth + this.target.size.w / 2,
+      this.target.position.x * canvasWidth +
+        this.target.size.w / 2 -
+        perspective,
       this.target.position.y * canvasHeight + this.target.size.h / 2
     );
-    ctx.closePath();
-    ctx.stroke();
+    ctx.lineTo(
+      this.target.position.x * canvasWidth +
+        this.target.size.w / 2 +
+        perspective,
+      this.target.position.y * canvasHeight + this.target.size.h / 2
+    );
+    ctx.lineTo(this.position.x + 10, this.position.y);
+    ctx.fill();
   }
 }
